2d3d79e7ad98032efe8eaf28ff887126ed1ad62e
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-10-30 12:36:32 -0700

    Fix: Add `release-it` to `devDependencies` when `release` workflow is selected

--- lib/helpers/create-tasks/create/releaseIt.ts
+++ lib/helpers/create-tasks/create/releaseIt.ts
@@ -4,11 +4,21 @@ import shouldWriteGithubWorkflow from '../prompts/shouldWriteGithubWorkflow'
 import { copyFile } from 'fs/promises'
 import { join } from 'path'
 import resDir from '../../resPath'
+import packageJsonTask, { PackageJsonEditor } from './packageJson'
+import resolvePackageVersions from '../../../resolvePackageVersions'
 
-const releaseIt: Task<void, [Set<Workflow>]> = {
-  dependencies: [shouldWriteGithubWorkflow],
-  fn: async workflows => {
-    if (workflows.has('release')) await copyFile(join(resDir, 'releaseIt.json'), '.release-it.json')
+const releaseIt: Task<void, [Set<Workflow>, PackageJsonEditor]> = {
+  dependencies: [shouldWriteGithubWorkflow, packageJsonTask],
+  fn: async (workflows, { beforeWrite, data }) => {
+    if (workflows.has('release')) {
+      beforeWrite.push((async () => {
+        Object.assign(data.devDependencies ?? (data.devDependencies = {}),
+          await resolvePackageVersions({
+            'release-it': '^14.9.0'
+          }))
+      })())
+      await copyFile(join(resDir, 'releaseIt.json'), '.release-it.json')
+    }
   }
 }
 
